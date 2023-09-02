01af64c959c505f18e50bdb9daccff856fb33ed2
CristhianMamani <cristhian.mamani@usil.pe>
2023-09-02 11:47:34 -0500

    actividad2

---
+++ actividad2/src/actividad2/acti23.java
@@ -0,0 +1,17 @@
+package actividad2;
+
+public class acti23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=1, y=2, z=3;
+		
+		if(x>y)
+		if(x>z)
+		  System.out.println("1111");
+		else
+		  System.out.println("2222");
+	   //LA RESPUESTA ES:
+     }
+}
