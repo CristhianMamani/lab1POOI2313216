b0725684161190134c365b2f42071c237e3f42a1
CristhianMamani <cristhian.mamani@usil.pe>
2023-09-02 11:51:59 -0500

    actividad7

---
+++ actividad7/src/actividad7/actividad7.java
@@ -0,0 +1,20 @@
+package actividad7;
+
+public class actividad7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1, y=2, z=3;
+		
+		if(++x > y++ || x-- > 0)
+			z++;
+		else
+			z--;
+		System.out.println(x+" "+y+" "+z);
+		//LA RESPUESTA ES: 1 3 4
+		
+		
+
+	}
+
+}
