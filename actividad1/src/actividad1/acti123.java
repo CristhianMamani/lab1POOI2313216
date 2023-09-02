92befe6408feba437fd9679c98404d46b33937b9
CristhianMamani <cristhian.mamani@usil.pe>
2023-09-02 11:32:53 -0500

    tarea1

---
+++ actividad1/src/actividad1/acti123.java
@@ -0,0 +1,13 @@
+package actividad1;
+
+public class acti123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if ( 6 < 6 * 5) 
+			System.out.print("Hello");
+		    System.out.print(" there");
+		    //LA RESPUESTA ES: Hello there
+	}
+
+}
