e4d241674beaf1a7acad186fb71815941fade815
CristhianMamani <cristhian.mamani@usil.pe>
2023-09-02 11:50:47 -0500

    actividad6

---
+++ actividad6/src/actividad6/activi6.java
@@ -0,0 +1,16 @@
+package actividad6;
+
+public class activi6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a1=100; int a2=200;
+		
+		if(a1>100 && a2<=200)
+		   System.out.print(a1+" "+a2+" "+(a1+a2));
+		else
+		   System.out.print(a1+" "+a2+" "+(2*a1-a2));
+		//LA RESPUESTA ES: 100 200 0
+		
+	}
+}
