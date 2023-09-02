bf4becdb48756faa62cdc859d1c476fd9a38470a
CristhianMamani <cristhian.mamani@usil.pe>
2023-09-02 11:46:46 -0500

    ACTIVIDAD11

---
+++ actividad11/src/actividad11/activi112.java
@@ -0,0 +1,46 @@
+package actividad11;
+
+import java.util.Scanner;
+
+public class activi112 {
+
+	public static void main(String[] args) 	{
+		// TODO Auto-generated method stub
+		
+	    Scanner obj = new Scanner(System.in);
+	    System.out.print("introduce: ");
+	    int n = obj.nextInt();
+	    
+	    
+	    switch (n) 	{
+	    case 1:
+	    	System.out.println("Lunes");
+	    	break;
+	    case 2:
+	    	System.out.println("Martes");
+	    	break;
+	    case 3:
+	    	System.out.println("Miercoles");
+	    	break;
+	    case 4:
+	        System.out.println("Jueves");
+	        break;
+	    case 5:
+	    	System.out.println("Viernes");
+	    	break;
+	    case 6:
+	    	System.out.println("Sábado");
+	    	break;
+	    case 7:
+	    	System.out.println("Domingo");
+	    	break;
+	    		//LA RESPUESTA ES : INTRODUCE:
+	    	
+	        
+	    }
+		
+	
+	
+	}
+
+}
