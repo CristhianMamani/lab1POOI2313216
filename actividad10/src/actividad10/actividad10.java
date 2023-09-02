ea5b99144bafeb4eb08750ee5cf304d87eac0120
CristhianMamani <cristhian.mamani@usil.pe>
2023-09-02 11:43:57 -0500

    actividad10

---
+++ actividad10/src/actividad10/actividad10.java
@@ -0,0 +1,21 @@
+package actividad10;
+
+public class actividad10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1, y=2, z=3;
+		
+		if (x <z)
+			System.out.println("*");
+		else if(x==z)
+			System.out.println("&");
+		else
+			System.out.println("$");
+		//LA RESPUESTA ES:*
+		
+			
+
+	}
+
+}
